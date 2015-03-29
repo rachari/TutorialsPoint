9aa4db7d4aa07788e43ec5af123ffc4d73afd957
Ranganath Achari <ranganath.achari@gmail.com>
2015-03-29 22:11:44 +0530

    nested switch example

---
+++ C/src/nested_switch.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/* Example of nested switch statement */
+
+int main()
+{
+	/* local variable definition */
+	int a = 100;
+	int b = 200;
+	
+	switch(a) {
+		case 100: 
+			printf("This is part of outer switch %d \n", a );
+			switch(b) {
+				case 200:
+					printf("This is part of inner switch %d \n", b );
+			}
+	}
+	printf("Exact value of a is : %d\n", a );
+	printf("Exact value of b is : %d\n", b );
+ 
+	return 0;
+}
