28cc5a8bc497270420ac7342972de752914cc0f5
Ranganath Achari <ranganath.achari@gmail.com>
2015-06-08 00:41:56 +0530

    while loop example

---
+++ C/src/while_loop.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    /* local variable declaration */
+    int a = 10;
+
+    /* while loop execution */
+    while ( a < 20 )
+    {
+        printf("Value of a: %d\n", a);
+        a++;
+    }
+    return 0;
+}
