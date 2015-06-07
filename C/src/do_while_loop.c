5ed477b3671467fa91f22a3eebeac166d1842541
Ranganath Achari <ranganath.achari@gmail.com>
2015-06-08 00:42:36 +0530

    do_while loop example

---
+++ C/src/do_while_loop.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    /* local variable declaration */
+    int a = 10;
+
+    /* do loop execution */
+    do
+    {
+        printf("Value of a: %d\n", a);
+        a++;
+    }
+    while (a < 20);
+    return 0;
+}
